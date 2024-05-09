93766a5ee5397f5f08f8f4c24d77f2f90be5a8e4
arrovain <arrovain@gmail.com>
2024-05-09 16:47:56 +0300

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <>hi</>;
+  return (
+    <div>
+      <Button>hi</Button>
+    </div>
+  );
 }
