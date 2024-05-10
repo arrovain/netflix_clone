dd46737db3aeefdfe0683133e153bc45551154ab
arrovain <arrovain@gmail.com>
2024-05-10 18:34:48 +0300

    error fix (nextConfig)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: [
+    {
+      protocol: "https",
+      host: "image.tmdb.org",
+      port: "",
+    },
+  ],
+};
 
 export default nextConfig;
