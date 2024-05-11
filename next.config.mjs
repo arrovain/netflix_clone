f0fb2691ebca2bef57746ed200369455060276e5
arrovain <arrovain@gmail.com>
2024-05-11 16:53:17 +0300

    genre

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: [
-    {
-      protocol: "https",
-      host: "image.tmdb.org",
-      port: "",
-    },
-  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        host: "image.tmdb.org",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
